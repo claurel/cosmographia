efbc3a64d197529da1594b98d072361c1d23a161
claurel <claurel@422cfe31-b723-5929-a464-a02b6640f278>
2011-06-14 22:22:06 +0000

    Added QObject wrappers for some VESTA classes

---
+++ trunk/src/main/qtwrapper/VisualizerObject.cpp
@@ -0,0 +1,33 @@
+// This file is part of Cosmographia.
+//
+// Copyright (C) 2011 Chris Laurel <claurel@gmail.com>
+//
+// Cosmographia is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// Cosmographia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with Cosmographia. If not, see <http://www.gnu.org/licenses/>.
+
+#include "VisualizerObject.h"
+#include <QDebug>
+
+using namespace vesta;
+
+
+VisualizerObject::VisualizerObject(Visualizer* visualizer, QObject* parent) :
+    QObject(parent),
+    m_visualizer(visualizer)
+{
+}
+
+
+VisualizerObject::~VisualizerObject()
+{
+}

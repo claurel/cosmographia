82bd56e36aab558256ce3ee70434fa993bafe19c
claurel <claurel@422cfe31-b723-5929-a464-a02b6640f278>
2011-07-28 17:00:00 +0000

    Fixed code to find data directory to work on Japanese versions of Mac OS X.
    git-svn-id: http://cosmographia.googlecode.com/svn/trunk@445 422cfe31-b723-5929-a464-a02b6640f278

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -12,6 +12,7 @@
 #include <QDir>
 #include <QMessageBox>
 #include <QDebug>
+#include <QDesktopServices>
 
 #ifdef Q_WS_MAC
 #include <CoreFoundation/CFBundle.h>
@@ -19,13 +20,26 @@
 
 #include "Cosmographia.h"
 
+#define MAS_DEPLOY 1
+
+
 int main(int argc, char *argv[])
 {
+    extern void qt_force_trolltech_settings_to_app_area(bool bVal);
+    qt_force_trolltech_settings_to_app_area(true);
+
     QApplication app(argc, argv);
 
+#if MAS_DEPLOY
+#else
     QCoreApplication::setOrganizationName("Periapsis Visual Software");
     QCoreApplication::setOrganizationDomain("periapsisvisual.com");
     QCoreApplication::setApplicationName("Cosmographia");
+#endif
+
+    // Useful when we need to know where the data files are:
+    //   qDebug() << QDesktopServices::storageLocation(QDesktopServices::CacheLocation);
+    //   qDebug() << QDesktopServices::storageLocation(QDesktopServices::DataLocation);
 
     // Set current directory so that we find the needed data files. On the Mac, we
     // just look in the app bundle. On other platforms we make some guesses, since we
@@ -33,13 +47,26 @@ int main(int argc, char *argv[])
     QString dataPath;
     bool foundData = true;
 #ifdef Q_WS_MAC
-    // On the Mac, load resources from the app bundle
-    CFURLRef appUrlRef = CFBundleCopyBundleURL(CFBundleGetMainBundle());
-    CFStringRef macPath = CFURLCopyFileSystemPath(appUrlRef, kCFURLPOSIXPathStyle);
-    const char *pathPtr = CFStringGetCStringPtr(macPath, CFStringGetSystemEncoding());
-    dataPath = QString(pathPtr) + "/Contents/Resources/data";
-    CFRelease(appUrlRef);
-    CFRelease(macPath);
+    // On the Mac, load resources from the app bundle. We first check for a directory
+    // in a location relative to the executable. If that fails, we'll use the Core
+    // Foundation bundle functions as recommended in the Qt docs. The first technique
+    // *should* always work.
+    QDir::setCurrent(QCoreApplication::applicationDirPath());
+    if (QDir("../Resources/data").exists())
+    {
+        dataPath = "../Resources/data";
+    }
+    else
+    {
+        // Note that there is a bug in one of the bundle functions that prevents this from
+        // working properly on a Japanese Mac OS X install.
+        CFURLRef appUrlRef = CFBundleCopyBundleURL(CFBundleGetMainBundle());
+        CFStringRef macPath = CFURLCopyFileSystemPath(appUrlRef, kCFURLPOSIXPathStyle);
+        const char *pathPtr = CFStringGetCStringPtr(macPath, CFStringGetSystemEncoding());
+        dataPath = QString(pathPtr) + "/Contents/Resources/data";
+        CFRelease(appUrlRef);
+        CFRelease(macPath);
+    }
 #else
     QDir::setCurrent(QCoreApplication::applicationDirPath());
     if (QDir("../data").exists())

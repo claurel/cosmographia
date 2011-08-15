fa722bee67cd64ba23bf8a8ec50e350d1c5d5ee1
claurel <claurel@422cfe31-b723-5929-a464-a02b6640f278>
2011-08-15 18:31:06 +0000

    Updated copyright in headers of VESTA particle system source files.
    git-svn-id: http://cosmographia.googlecode.com/svn/trunk@480 422cfe31-b723-5929-a464-a02b6640f278

--- thirdparty/vesta/particlesys/BoxGenerator.h
+++ thirdparty/vesta/particlesys/BoxGenerator.h
@@ -1,25 +1,12 @@
-// BoxGenerator.h
-//
-// Copyright (C) 2010 Chris Laurel <claurel@gmail.com>
-//
-// VESTA is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2 of the License, or (at your option) any later version.
-//
-// Alternatively, you can redistribute it and/or
-// modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of
-// the License, or (at your option) any later version.
-//
-// VESTA is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License or the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public
-// License and a copy of the GNU General Public License along with
-// VESTA. If not, see <http://www.gnu.org/licenses/>.
+/*
+ * $Revision$ $Date$
+ *
+ * Copyright by Astos Solutions GmbH, Germany
+ *
+ * This file is published under the Astos Solutions Free Public License
+ * For details on copyright and terms of use see 
+ * http://www.astos.de/Astos_Solutions_Free_Public_License.html
+ */
 
 #ifndef _VESTA_BOX_GENERATOR_H_
 #define _VESTA_BOX_GENERATOR_H_

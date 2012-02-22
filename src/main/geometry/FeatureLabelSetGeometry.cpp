1b150cb23b0c782e4cc579749b9e232d013db3c8
claurel <claurel@422cfe31-b723-5929-a464-a02b6640f278>
2012-02-22 20:00:38 +0000

    FeatureLabelSet improvements: - Don't let labels be partially occluded by the planet - Per-label coloring
    git-svn-id: http://cosmographia.googlecode.com/svn/trunk@651 422cfe31-b723-5929-a464-a02b6640f278

--- src/main/geometry/FeatureLabelSetGeometry.cpp
+++ src/main/geometry/FeatureLabelSetGeometry.cpp
@@ -43,46 +43,57 @@ FeatureLabelSetGeometry::~FeatureLabelSetGeometry()
 void
 FeatureLabelSetGeometry::render(RenderContext& rc, double /* clock */) const
 {
+    const float visibleSizeThreshold = 20.0f; // in pixels
+
     // Render during the opaque pass if opaque or during the translucent pass if not.
     if (rc.pass() == RenderContext::TranslucentPass)
     {
-        AlignedEllipsoid testEllipsoid(m_occludingEllipsoid.semiAxes() * 0.999);
-        Vector3f ellipsoidSemiAxes = testEllipsoid.semiAxes().cast<float>();
-
         // Get the position of the camera in the body-fixed frame of the labeled object
         Transform3f inv = Transform3f(rc.modelview().inverse(Affine)); // Assuming an affine modelview matrix
         Vector3f cameraPosition = inv.translation();
+        float overallPixelSize = boundingSphereRadius() / (rc.pixelSize() * cameraPosition.norm());
 
-        Vector3f viewDir = -cameraPosition.normalized();
-        double distanceToEllipsoid = 0.0;
-        TestRayEllipsoidIntersection(cameraPosition, viewDir, ellipsoidSemiAxes, &distanceToEllipsoid);
-
-        Hyperplane<float, 3> labelPlane(viewDir, cameraPosition + viewDir * float(distanceToEllipsoid));
-
-        for (vector<Feature>::const_iterator iter = m_features.begin(); iter != m_features.end(); ++iter)
+        // Only draw individual labels if the overall projected size of the set exceeds the threshold
+        if (overallPixelSize > visibleSizeThreshold)
         {
-            Vector3f r = iter->position - cameraPosition;
+            // Labels are treated as either completely visible or completely occluded. A label is
+            // visible when the labeled point isn't blocked by the occluding ellipsoid.
+            AlignedEllipsoid testEllipsoid(m_occludingEllipsoid.semiAxes() * 0.999);
+            Vector3f ellipsoidSemiAxes = testEllipsoid.semiAxes().cast<float>();
 
-            Vector3f labelPosition = labelPlane.projection(iter->position);
-            float k = -(labelPlane.normal().dot(cameraPosition) + labelPlane.offset()) / (labelPlane.normal().dot(r));
-            labelPosition = cameraPosition + k * r;
+            Vector3f viewDir = -cameraPosition.normalized();
+            double distanceToEllipsoid = 0.0;
+            TestRayEllipsoidIntersection(cameraPosition, viewDir, ellipsoidSemiAxes, &distanceToEllipsoid);
 
-            rc.pushModelView();
-            rc.translateModelView(labelPosition);
-            float featureDistance = rc.modelview().translation().norm();
-            float pixelSize = iter->size / (rc.pixelSize() * featureDistance);
+            // We don't want labels partially hidden by the planet ellipsoid, so we'll project them onto a
+            // plane that lies just in front of the planet ellipsoid and which is parallel to the view plane
+            Hyperplane<float, 3> labelPlane(viewDir, cameraPosition + viewDir * float(distanceToEllipsoid));
 
-            float d = r.norm();
-            r /= d;
-            double t = 0.0;
-            TestRayEllipsoidIntersection(cameraPosition, r, ellipsoidSemiAxes, &t);
-
-            if (pixelSize > 10.0f && d < t)
+            for (vector<Feature, Eigen::aligned_allocator<Feature> >::const_iterator iter = m_features.begin(); iter != m_features.end(); ++iter)
             {
-                rc.drawText(Vector3f::Zero(), iter->label, m_font.ptr(), Spectrum(1.0f, 1.0f, 1.0f), 1.0f);
-            }
+                Vector3f r = iter->position - cameraPosition;
 
-            rc.popModelView();
+                Vector3f labelPosition = labelPlane.projection(iter->position);
+                float k = -(labelPlane.normal().dot(cameraPosition) + labelPlane.offset()) / (labelPlane.normal().dot(r));
+                labelPosition = cameraPosition + k * r;
+
+                rc.pushModelView();
+                rc.translateModelView(labelPosition);
+                float featureDistance = rc.modelview().translation().norm();
+                float pixelSize = iter->size / (rc.pixelSize() * featureDistance);
+
+                float d = r.norm();
+                r /= d;
+                double t = 0.0;
+                TestRayEllipsoidIntersection(cameraPosition, r, ellipsoidSemiAxes, &t);
+
+                if (pixelSize > visibleSizeThreshold && d < t)
+                {
+                    rc.drawText(Vector3f::Zero(), iter->label, m_font.ptr(), iter->color, 1.0f);
+                }
+
+                rc.popModelView();
+            }
         }
     }
 }
@@ -104,12 +115,13 @@ FeatureLabelSetGeometry::boundingSphereRadius() const
   * \param radius size of the feature in units of kilometers
   */
 void
-FeatureLabelSetGeometry::addFeature(const string& label, const Vector3f& position, float radius)
+FeatureLabelSetGeometry::addFeature(const string& label, const Vector3f& position, float radius, const Spectrum& color)
 {
     Feature feature;
     feature.label = label;
     feature.position = position;
     feature.size = radius;
+    feature.color = color;
 
     m_features.push_back(feature);
 

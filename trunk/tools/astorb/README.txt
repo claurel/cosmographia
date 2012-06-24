87242c464c719bcb153f5c346f0839fe57054225
claurel <claurel@422cfe31-b723-5929-a464-a02b6640f278>
2012-06-24 00:24:23 +0000

    Updated astorb2bin tool: - Added option to load discovery datess from anauxilliary catalog - Handle special provisional designations for the Palomar-Leiden and Trojan surveys

--- trunk/tools/astorb/README.txt
+++ trunk/tools/astorb/README.txt
@@ -8,9 +8,28 @@ files in the format described here:
 
 === astorb2bin ===
 
-Command line: astorb2bin <input file> <output file>
-
 Converts an ASCII astorb file to the binary format used by Cosmographia.
+
+Usage: astorb2bin.py [options]
+
+Options:
+  -h, --help                  show this help message and exit
+  -o FILE, --out=FILE         Write binary catalog to FILE
+  -i FILE, --in=FILE          Read asteroid orbital data from FILE
+  -d FILE, --discovery=FILE   Read discovery dates from FILE
+
+Example:
+   astorb2bin.py -i astorb.dat -o allasteroids.bin -d NumberedMPs.txt
+
+One of the fields in Cosmographia's binary format for asteroid orbits is
+the discovery date. Objects will be plotted only when the simulating time
+is after the discovery date. The iastorb inputcatalog doesn't contain
+discovery dates so they are inferred from the provisional designation or
+looked up in an auxilliary catalog of discovery circumstances. The file
+NumberedMPs.txt in this directory may be used, but it is better to grab
+the latest catalog from the Minor Planet Center:
+
+   http://www.minorplanetcenter.net/iau/lists/NumberedMPs000001.html
 
 
 === astorb2json ===
--- trunk/tools/astorb/astorb2bin.py
+++ trunk/tools/astorb/astorb2bin.py
@@ -13,18 +13,52 @@ import sys
 import datetime
 import struct
 import re
+from optparse import OptionParser
 
-astorb = open(sys.argv[1], 'r')
-out = open(sys.argv[2], 'wb')
+parser = OptionParser()
+parser.add_option("-o", "--out", dest="outfile",
+                  help="Write binary catalog to FILE", metavar="FILE")
+parser.add_option("-i", "--in", dest="infile",
+                  help="Read asteroid orbital data from FILE", metavar="FILE")
+parser.add_option("-d", "--discovery", dest="discfilename",
+                  help="Read discovery dates from FILE", metavar="FILE")
 
+(options, args) = parser.parse_args()
+
+astorb = sys.stdin
+if options.infile:
+    astorb = open(options.infile, 'r')
+
+out = sys.stdout
+if options.outfile:
+    out = open(options.outfile, 'wb')
+
+discdates = {}
+if options.discfilename:
+    discfile = open(options.discfilename, 'r')
+    for record in discfile:
+        num = int(record[0:8].strip('() '))
+        year = record[41:45]
+        month = record[46:48]
+        day = record[49:51]
+        date = datetime.datetime(int(year), int(month), int(day), 12, 0, 0)
+        discdates[num] = date
+        
 j2000 = datetime.datetime(2000, 1, 1, 12, 0, 0)
 provisionalDesignation = re.compile('[0-9]{4,4} [A-Z]{2,2}[0-9]*')
 
 for record in astorb:
+    num = record[0:6].strip()
+    if num:
+        num = int(num)
+    else:
+        num = 0
+
     id = record[7:26].strip()
     epochYear = record[106:110]
     epochMonth = record[110:112]
     epochDay = record[112:114]
+
     date = datetime.datetime(int(epochYear), int(epochMonth), int(epochDay), 12, 0, 0)
     dt = (date - j2000)
 
@@ -37,13 +71,32 @@ for record in astorb:
 
     epoch = 2451545.0 + dt.days + dt.seconds / 86400.0
 
-    discoveryDate = 0
-
-    # Extract the approximate discovery date from the provision designation
-    if provisionalDesignation.match(id):
-        month = (ord(id[5]) - 65) / 2 + 1
-        discoveryDate = 2451545 + (float(id[0:4]) - 2000) * 365.25 + month * 30.0
-
+    discoveryDate = discdates.get(num, False)
+    if discoveryDate:
+        # print num, id, discoveryDate
+        dt = (discoveryDate - j2000)
+        discoveryDate = 2451545.0 + dt.days + dt.seconds / 86400.0
+    else:
+        # Extract the approximate discovery date from the provision designation
+        discoveryDate = 0
+        if provisionalDesignation.match(id):
+            month = (ord(id[5]) - 65) / 2 + 1
+            discoveryDate = 2451545 + (float(id[0:4]) - 2000) * 365.25 + month * 30.0
+        elif id.endswith("P-L"):
+            # Palomar-Leiden survey, 1960
+            discoveryDate = 2451545 + (1960 - 2000) * 365.25
+        elif id.endswith("T-1"):
+            # First Trojan survey, 1971
+            discoveryDate = 2451545 + (1971 - 2000) * 365.25
+        elif id.endswith("T-2"):
+            # Second Trojan survey, 1973
+            discoveryDate = 2451545 + (1973 - 2000) * 365.25
+        elif id.endswith("T-3"):
+            # Third Trojan survey, 1977
+            discoveryDate = 2451545 + (1977 - 2000) * 365.25
+        else:
+            print "Bad identifier: ", id
+    
     binrecord = struct.pack('>ffffffdf', sma, eccentricity, inclination, ascendingNode, argOfPeriapsis, meanAnomaly, epoch, discoveryDate)
     out.write(binrecord)
     

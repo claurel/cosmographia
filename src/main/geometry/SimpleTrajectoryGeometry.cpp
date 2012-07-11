b11f830ca667826f9269227c724ac99bb7cf64ad
claurel <claurel@422cfe31-b723-5929-a464-a02b6640f278>
2012-07-11 01:03:51 +0000

    Added SimpleTrajectoryGeometry class, a replacement for TrajectoryGeometry that is better suited for mobile CPUs.
    git-svn-id: http://cosmographia.googlecode.com/svn/trunk@762 422cfe31-b723-5929-a464-a02b6640f278

---
+++ src/main/geometry/SimpleTrajectoryGeometry.cpp
@@ -0,0 +1,359 @@
+// This file is part of Cosmographia.
+//
+// Copyright (C) 2012 Chris Laurel <claurel@gmail.com>
+//
+// Cosmographia is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// Cosmographia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with Cosmographia. If not, see <http://www.gnu.org/licenses/>.
+
+#include "SimpleTrajectoryGeometry.h"
+#include <vesta/RenderContext.h>
+#include <algorithm>
+#include <iomanip>
+
+using namespace vesta;
+using namespace Eigen;
+
+
+
+SimpleTrajectoryGeometry::SimpleTrajectoryGeometry() :
+    m_color(Spectrum(1.0f, 1.0f, 1.0f)),
+    m_opacity(1.0f),
+    m_startTime(0.0),
+    m_endTime(0.0),
+    m_boundingRadius(0.0),
+    m_displayedPortion(TrajectoryGeometry::Entire),
+    m_windowDuration(0.0),
+    m_windowLead(0.),
+    m_fadeFraction(0.0),
+    m_lineWidth(1.0f)
+{
+    setClippingPolicy(SplitToPreventClipping);
+}
+
+
+SimpleTrajectoryGeometry::~SimpleTrajectoryGeometry()
+{
+}
+
+
+Eigen::Vector3d
+SimpleTrajectoryGeometry::interpolateSamples(double t, double dt, const TrajectorySample& s0, const TrajectorySample& s1) const
+{
+    // Use cubic Hermite interpolation between trajectory samples; linear interpolation is too inaccurate and
+    // results in the end of the trajectory plot being noticeably offset from current position of the object.
+    Matrix<double, 3, 4> m;
+    m.col(0) = s0.position;
+    m.col(1) = s0.velocity * dt;
+    m.col(2) = 3.0 * (s1.position - s0.position) - (2.0 * s0.velocity + s1.velocity) * dt;
+    m.col(3) = 2.0 * (s0.position - s1.position) + (s1.velocity + s0.velocity) * dt;
+    return m * Vector4d(1.0f, t, t * t, t * t * t);
+}
+
+
+void
+SimpleTrajectoryGeometry::render(RenderContext& rc, double clock) const
+{
+    double t0 = firstSampleTime();
+    double t1 = lastSampleTime();
+
+    double fadeRate = 0.0;
+    double fadeStartTime = 0.0;
+    double fadeStartValue = 1.0;
+
+    // Only draw during the appropriate render pass
+    if ((rc.pass() == RenderContext::OpaquePass && !isOpaque()) ||
+        (rc.pass() == RenderContext::TranslucentPass && isOpaque()))
+    {
+        return;
+    }
+    
+    if (displayedPortion() == TrajectoryGeometry::WindowBeforeCurrentTime)
+    {
+        t0 = clock + m_windowLead - m_windowDuration;
+        t1 = clock + m_windowLead;
+
+        fadeStartTime = t0;
+        fadeStartValue = 0.0;
+        double fadeEndTime = fadeStartTime + m_windowDuration * m_fadeFraction;
+        fadeRate = 1.0 / (fadeEndTime - fadeStartTime);
+    }
+
+    // Nothing to be drawn
+    if (t1 <= t0)
+    {
+        return;
+    }
+
+    // Basic opacity of the plot. It may be modified based on three things:
+    //   - Approximate size in pixels of the trajectory (small trajectories will fade out)
+    //   - Distance from the camera to the 'front' (usually the current position of the oribiting body)
+    //   - 'Age' of the trajectory: typically the most recent portions are drawn more opaque than the
+    //        older parts. This is handled by setting per-vertex colors.
+    float opacity = 0.99f * m_opacity;
+
+    const float sizeFadeStart = 30.0f;
+    const float sizeFadeEnd = 15.0f;
+    float pixelSize = boundingSphereRadius() / (rc.modelview().translation().norm() * rc.pixelSize());
+    if (pixelSize < sizeFadeStart)
+    {
+        opacity *= std::max(0.0f, (pixelSize - sizeFadeEnd) / (sizeFadeStart - sizeFadeEnd));
+    }
+
+    if (opacity <= 0.0f)
+    {
+        // Complete fade out; no need to draw anything.
+        return;
+    }
+
+    rc.pushModelView();
+    if (m_frame.isValid())
+    {
+        rc.rotateModelView(m_frame->orientation(clock).cast<float>());
+    }
+
+    TrajectoryVertex vertex;
+    vertex.color[0] = (unsigned char) (m_color.red() * 255.99f);
+    vertex.color[1] = (unsigned char) (m_color.green() * 255.99f);
+    vertex.color[2] = (unsigned char) (m_color.blue() * 255.99f);
+    vertex.color[3] = 255;
+
+    m_vertexData.clear();
+
+    unsigned int sampleIndex = 0;
+    while (sampleIndex < m_samples.size() && t0 > m_samples[sampleIndex].timeTag)
+    {
+        ++sampleIndex;
+    }
+
+    if (sampleIndex > 0 && sampleIndex < m_samples.size())
+    {
+        double dt = m_samples[sampleIndex].timeTag - m_samples[sampleIndex - 1].timeTag;
+        double t = (t0 - m_samples[sampleIndex - 1].timeTag) / dt;
+        Vector3d interpolated = interpolateSamples(t, dt, m_samples[sampleIndex - 1], m_samples[sampleIndex]);
+        float alpha = std::max(0.0f, std::min(1.0f, float(fadeStartValue + (t0 - fadeStartTime) * fadeRate)));
+
+        vertex.position = interpolated.cast<float>();
+        vertex.color[3] = (unsigned char) (alpha * 255.99f);
+        m_vertexData.push_back(vertex);
+    }
+
+    while (sampleIndex < m_samples.size() && t1 > m_samples[sampleIndex].timeTag)
+    {
+        float alpha = std::max(0.0f, std::min(1.0f, float(fadeStartValue + (m_samples[sampleIndex].timeTag - fadeStartTime) * fadeRate)));
+
+        vertex.position = m_samples[sampleIndex].position.cast<float>();
+        vertex.color[3] = (unsigned char) (alpha * 255.99f);
+        m_vertexData.push_back(vertex);
+
+        ++sampleIndex;
+    }
+
+    if (sampleIndex > 0 && sampleIndex < m_samples.size())
+    {
+        double dt = m_samples[sampleIndex].timeTag - m_samples[sampleIndex - 1].timeTag;
+        double t = (t1 - m_samples[sampleIndex - 1].timeTag) / dt;
+        Vector3d interpolated = interpolateSamples(t, dt, m_samples[sampleIndex - 1], m_samples[sampleIndex]);
+        float alpha = std::max(0.0f, std::min(1.0f, float(fadeStartValue + (t1 - fadeStartTime) * fadeRate)));
+
+        vertex.position = interpolated.cast<float>();
+        vertex.color[3] = (unsigned char) (alpha * 255.99f);
+        m_vertexData.push_back(vertex);
+    }
+
+    // Fade trajectory based on size
+    Vector3f frontPosition = rc.modelview() * vertex.position;
+    float frontDistance = frontPosition.norm();
+
+    // Fade trajectory based on distance to front point. This is helpful because the simple trajectory model
+    // is not precise, and fading hides the discrepancy between the plot and the body's current position.
+    const float fadeStart = 0.04f;
+    const float fadeFinish = 0.01f;
+    if (frontDistance < fadeStart * boundingSphereRadius())
+    {
+        opacity *= std::max(0.0f, (frontDistance / boundingSphereRadius() - fadeFinish) / (fadeStart - fadeFinish));
+    }
+
+    Material material;
+    material.setDiffuse(Spectrum::White());
+    material.setOpacity(opacity);
+    rc.bindMaterial(&material);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    if (m_vertexData.size() > 1 && opacity > 0.0f)
+    {
+        rc.bindVertexArray(VertexSpec::PositionColor, m_vertexData[0].position.data(), sizeof(TrajectoryVertex));
+        rc.drawPrimitives(PrimitiveBatch(PrimitiveBatch::LineStrip, m_vertexData.size() - 1, 0));
+        rc.unbindVertexArray();
+    }
+
+    glDisable(GL_BLEND);
+
+    rc.popModelView();
+}
+
+
+void
+SimpleTrajectoryGeometry::addSample(double t, const vesta::StateVector& s)
+{
+    TrajectorySample sample;
+    sample.position = s.position();
+    sample.velocity = s.velocity();
+    sample.timeTag = t;
+
+    if (m_samples.empty())
+    {
+        m_startTime = t;
+        m_endTime = t;
+        m_samples.push_back(sample);
+    }
+    else if (t > m_samples.back().timeTag)
+    {
+        m_samples.push_back(sample);
+    }
+    else if (sample.timeTag < m_samples.front().timeTag)
+    {
+        m_samples.push_front(sample);
+    }
+    else
+    {
+        // Can't add sample in middle
+        return;
+    }
+
+    m_boundingRadius = std::max(m_boundingRadius, sample.position.norm());
+}
+
+
+void
+SimpleTrajectoryGeometry::clearSamples()
+{
+    m_boundingRadius = 0.0;
+    m_samples.clear();
+    m_startTime = 0.0;
+    m_endTime = 0.0;
+}
+
+
+void
+SimpleTrajectoryGeometry::computeSamples(const vesta::TrajectoryPlotGenerator* generator, double startTime, double endTime, unsigned int stepCount)
+{
+    clearSamples();
+    double t0 = std::max(generator->startTime(), startTime);
+    double t1 = std::min(generator->endTime(), endTime);
+    double dt = t1 - t0;
+    if (dt <= 0.0 || stepCount == 0)
+    {
+        return;
+    }
+
+    double invStep = 1.0 / double(stepCount);
+
+    for (unsigned int i = 0; i <= stepCount; ++i)
+    {
+        double t = t0 + dt * (i * invStep);
+        addSample(t, generator->state(t));
+    }
+}
+
+
+void
+SimpleTrajectoryGeometry::updateSamples(const vesta::TrajectoryPlotGenerator* generator, double startTime, double endTime, unsigned int stepCount)
+{
+    double dt = endTime - startTime;
+    if (dt <= 0.0 || stepCount == 0)
+    {
+        return;
+    }
+
+    double stepTime = dt / std::max(1u, stepCount - 1);
+    double t0 = std::max(generator->startTime(), startTime - stepTime);
+    double t1 = std::min(generator->endTime(), endTime + stepTime);
+
+    // We need to recompute everything when there are no samples already or when the time range is
+    // disjoint with the current sampled time range.
+    if (m_samples.empty() || timeRangeDisjointWithSampleTimeRange(t0, t1))
+    {
+        computeSamples(generator, t0, t1, stepCount);
+    }
+    else
+    {
+        // Add samples at beginning
+        if (t0 < firstSampleTime())
+        {
+            for (double t = firstSampleTime() - stepTime; t > t0; t -= stepTime)
+            {
+                t = std::max(t, t0);
+                addSample(t, generator->state(t));
+            }
+        }
+
+        // Add samples at end
+        if (t1 > lastSampleTime())
+        {
+            for (double t = lastSampleTime() + stepTime; t < t1; t += stepTime)
+            {
+                t = std::min(t, t1);
+                addSample(t, generator->state(t));
+            }
+        }
+
+        removeSamplesBeforeTime(t0);
+        removeSamplesAfterTime(t1);
+    }
+
+    m_startTime = t0;
+    m_endTime = t1;
+}
+
+
+void
+SimpleTrajectoryGeometry::removeSamplesBeforeTime(double t)
+{
+    while (!m_samples.empty() && m_samples.front().timeTag < t)
+    {
+        m_samples.pop_front();
+    }
+}
+
+
+void
+SimpleTrajectoryGeometry::removeSamplesAfterTime(double t)
+{
+    while (!m_samples.empty() && m_samples.back().timeTag > t)
+    {
+        m_samples.pop_back();
+    }
+}
+
+
+double
+SimpleTrajectoryGeometry::firstSampleTime() const
+{
+    return m_samples.empty() ? 0 : m_samples.front().timeTag;
+}
+
+
+double
+SimpleTrajectoryGeometry::lastSampleTime() const
+{
+    return m_samples.empty() ? 0 : m_samples.back().timeTag;
+}
+
+
+bool
+SimpleTrajectoryGeometry::timeRangeDisjointWithSampleTimeRange(double startTime, double endTime) const
+{
+     return endTime <= firstSampleTime() || startTime >= lastSampleTime();
+}
---
+++ src/main/geometry/SimpleTrajectoryGeometry.h
@@ -0,0 +1,167 @@
+// This file is part of Cosmographia.
+//
+// Copyright (C) 2012 Chris Laurel <claurel@gmail.com>
+//
+// Cosmographia is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// Cosmographia is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with Cosmographia. If not, see <http://www.gnu.org/licenses/>.
+
+#include <vesta/TrajectoryGeometry.h>
+#include <Eigen/Core>
+#include <deque>
+#include <vector>
+
+
+class SimpleTrajectoryGeometry : public vesta::Geometry
+{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    SimpleTrajectoryGeometry();
+    virtual ~SimpleTrajectoryGeometry();
+
+    void render(vesta::RenderContext& rc, double clock) const;
+
+    float boundingSphereRadius() const
+    {
+        return m_boundingRadius * 1.05f;
+    }
+
+    virtual bool isOpaque() const
+    {
+        return m_opacity >= 1.0f;
+    }
+
+    vesta::Frame* frame() const
+    {
+        return m_frame.ptr();
+    }
+
+    void setFrame(vesta::Frame* frame)
+    {
+        m_frame = frame;
+    }
+
+    vesta::Spectrum color() const
+    {
+        return m_color;
+    }
+
+    void setColor(const vesta::Spectrum& color)
+    {
+        m_color = color;
+    }
+
+    float opacity() const
+    {
+        return m_opacity;
+    }
+
+    void setOpacity(float opacity)
+    {
+        m_opacity = opacity;
+    }
+
+    void addSample(double t, const vesta::StateVector& s);
+    void clearSamples();
+    void computeSamples(const vesta::TrajectoryPlotGenerator* generator, double startTime, double endTime, unsigned int stepCount);
+    void updateSamples(const vesta::TrajectoryPlotGenerator* generator, double startTime, double endTime, unsigned int stepCount);
+
+    vesta::TrajectoryGeometry::TrajectoryPortion displayedPortion() const
+    {
+        return m_displayedPortion;
+    }
+
+    void setDisplayedPortion(vesta::TrajectoryGeometry::TrajectoryPortion portion)
+    {
+        m_displayedPortion = portion;
+    }
+
+    double windowDuration() const
+    {
+        return m_windowDuration;
+    }
+
+    void setWindowDuration(double duration)
+    {
+        m_windowDuration = duration;
+    }
+
+    double windowLead() const
+    {
+        return m_windowLead;
+    }
+
+    void setWindowLead(double duration)
+    {
+        m_windowLead = duration;
+    }
+
+    double fadeFraction() const
+    {
+        return m_fadeFraction;
+    }
+
+    void setFadeFraction(double fadeFraction)
+    {
+        m_fadeFraction = fadeFraction;
+    }
+
+    float lineWidth() const
+    {
+        return m_lineWidth;
+    }
+
+    void setLineWidth(float width)
+    {
+        m_lineWidth = width;
+    }
+
+private:
+    struct TrajectorySample
+    {
+        Eigen::Vector3d position;
+        Eigen::Vector3d velocity;
+        double timeTag;
+    };
+
+    struct TrajectoryVertex
+    {
+        Eigen::Vector3f position;
+        unsigned char color[4];
+    };
+
+    void removeSamplesBeforeTime(double t);
+    void removeSamplesAfterTime(double t);
+    double firstSampleTime() const;
+    double lastSampleTime() const;
+    bool timeRangeDisjointWithSampleTimeRange(double startTime, double endTime) const;
+    Eigen::Vector3d interpolateSamples(double t, double dt, const TrajectorySample& s0, const TrajectorySample& s1) const;
+
+private:
+    vesta::counted_ptr<vesta::Frame> m_frame;
+    vesta::Spectrum m_color;
+    float m_opacity;
+    double m_startTime;
+    double m_endTime;
+    double m_boundingRadius;
+    vesta::TrajectoryGeometry::TrajectoryPortion m_displayedPortion;
+    double m_windowDuration;
+    double m_windowLead;
+    double m_fadeFraction;
+    float m_lineWidth;
+
+    std::deque<TrajectorySample> m_samples;
+    mutable std::vector<TrajectoryVertex> m_vertexData;
+};
+
+
